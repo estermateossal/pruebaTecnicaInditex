57b546e3ef33f292311079a6756f4ba190b41fa8
ESTER MATEOS SALDAÑA <estermateossal@gmail.com>
2022-10-25 00:58:07 +0200

    Refactor LoadDatabase

--- src/main/java/com/prueba/tecnica/inditex/repository/LoadDatabase.java
+++ src/main/java/com/prueba/tecnica/inditex/repository/LoadDatabase.java
@@ -1,6 +1,6 @@
 package com.prueba.tecnica.inditex.repository;
 
-import com.prueba.tecnica.inditex.model.PriceByDate;
+import com.prueba.tecnica.inditex.model.PriceByDateModel;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,36 +16,29 @@ class LoadDatabase {
   @Bean
   CommandLineRunner initDatabase(PriceByDateRepository repository) {
     return args -> {
-      log.info("Preloading (1/4) " + repository.save(PriceByDate.builder()
-          .brandId(1)
-          .startDate(parseDate("2020-06-14-00.00.00"))
-          .endDate(parseDate("2020-12-31-23.59.59"))
-          .priceList(1).productId(35455L).priority(false).price(35.50).curr("EUR").build()));
+      SimpleDateFormat simpleDateFormat =  new SimpleDateFormat("yyyy-MM-dd-hh.mm.ss");
 
-      log.info("Preloading (2/4) " + repository.save(PriceByDate.builder()
+      log.info("Preloading (1/4) " + repository.save(PriceByDateModel.builder()
           .brandId(1)
-          .startDate(parseDate("2020-06-14-15.00.00"))
-          .endDate(parseDate("2020-06-14-18.30.00"))
-          .priceList(2).productId(35455L).priority(true).price(25.45).curr("EUR").build()));
-      log.info("Preloading (3/4) " + repository.save(PriceByDate.builder()
+          .startDate(simpleDateFormat.parse("2020-06-14-00.00.00"))
+          .endDate(simpleDateFormat.parse("2020-12-31-23.59.59"))
+          .priceList(1).productId(35455).priority(0).price(35.50).curr("EUR").build()));
+      log.info("Preloading (2/4) " + repository.save(PriceByDateModel.builder()
           .brandId(1)
-          .startDate(parseDate("2020-06-15-00.00.00"))
-          .endDate(parseDate("2020-06-15-11.00.00"))
-          .priceList(3).productId(35455L).priority(false).price(30.50).curr("EUR").build()));
-      log.info("Preloading (4/4) " + repository.save(PriceByDate.builder()
+          .startDate(simpleDateFormat.parse("2020-06-14-15.00.00"))
+          .endDate(simpleDateFormat.parse("2020-06-14-18.30.00"))
+          .priceList(2).productId(35455).priority(1).price(25.45).curr("EUR").build()));
+      log.info("Preloading (3/4) " + repository.save(PriceByDateModel.builder()
           .brandId(1)
-          .startDate(parseDate("2020-06-15-16.00.00"))
-          .endDate(parseDate("2020-12-31-23.59.59"))
-          .priceList(4).productId(35455L).priority(true).price(38.95).curr("EUR").build()));
+          .startDate(simpleDateFormat.parse("2020-06-15-00.00.00"))
+          .endDate(simpleDateFormat.parse("2020-06-15-11.00.00"))
+          .priceList(3).productId(35455).priority(1).price(30.50).curr("EUR").build()));
+      log.info("Preloading (4/4) " + repository.save(PriceByDateModel.builder()
+          .brandId(1)
+          .startDate(simpleDateFormat.parse("2020-06-15-16.00.00"))
+          .endDate(simpleDateFormat.parse("2020-12-31-23.59.59"))
+          .priceList(4).productId(35455).priority(1).price(38.95).curr("EUR").build()));
     };
-  }
-
-  private static Date parseDate(String date) {
-    try {
-      return new SimpleDateFormat("yyyy-MM-dd-hh.mm.ss").parse(date);
-    } catch (ParseException e) {
-      return null;
-    }
   }
 
 }

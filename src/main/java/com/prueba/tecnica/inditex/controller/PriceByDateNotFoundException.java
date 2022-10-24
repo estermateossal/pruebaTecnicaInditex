e7dd0b41b72600d428892978b5df15d3de85e995
ESTER MATEOS SALDAÑA <estermateossal@gmail.com>
2022-10-25 01:08:45 +0200

    Added new NotFoundException

--- src/main/java/com/prueba/tecnica/inditex/controller/PriceByDateNotFoundException.java
+++ src/main/java/com/prueba/tecnica/inditex/controller/PriceByDateNotFoundException.java
@@ -5,4 +5,9 @@ public class PriceByDateNotFoundException extends RuntimeException {
   PriceByDateNotFoundException(Long id) {
     super("Could not find price by date " + id);
   }
+
+  PriceByDateNotFoundException(String date, Integer productId, Integer brandId) {
+    super("Could not find price by date " + date + " " + productId + " " + brandId);
+  }
+
 }

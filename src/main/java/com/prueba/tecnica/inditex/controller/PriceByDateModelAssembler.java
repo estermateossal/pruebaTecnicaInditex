d5f338c2008a90e1af861575ed0873eaddf19ba6
ESTER MATEOS SALDAÑA <estermateossal@gmail.com>
2022-10-25 01:10:27 +0200

    Update Assembler response

--- src/main/java/com/prueba/tecnica/inditex/controller/PriceByDateModelAssembler.java
+++ src/main/java/com/prueba/tecnica/inditex/controller/PriceByDateModelAssembler.java
@@ -3,19 +3,20 @@ package com.prueba.tecnica.inditex.controller;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-import com.prueba.tecnica.inditex.model.PriceByDate;
+import com.prueba.tecnica.inditex.dto.PriceByDateDTO;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PriceByDateModelAssembler implements
-    RepresentationModelAssembler<PriceByDate, EntityModel<PriceByDate>> {
+public class PriceByDateModelAssembler
+    implements
+    RepresentationModelAssembler<PriceByDateDTO, EntityModel<PriceByDateDTO>> {
 
   @Override
-  public EntityModel<PriceByDate> toModel(PriceByDate priceByDate) {
-    return EntityModel.of(priceByDate, //
-        linkTo(methodOn(PriceByDateController.class).one(priceByDate.getId())).withSelfRel(),
+  public EntityModel<PriceByDateDTO> toModel(PriceByDateDTO priceByDateModel) {
+    return EntityModel.of(priceByDateModel,
+        linkTo(methodOn(PriceByDateController.class).one(priceByDateModel.getId())).withSelfRel(),
         linkTo(methodOn(PriceByDateController.class).all()).withRel("pricesByDate"));
   }
 }
